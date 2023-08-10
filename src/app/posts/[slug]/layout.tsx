f90929b5be14400821f08178df420f202f185969
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-08-10 16:12:14 -0600

    update markdown parsing and add styles

--- src/app/posts/[slug]/layout.tsx
+++ src/app/posts/[slug]/layout.tsx
@@ -1,3 +1,5 @@
+import "./post.css"
+
 export default function BlogPostLayout({children, }: {children: React.ReactNode}) {
     return (
         <article className="py-4 mx-auto prose dark:prose-dark min-w-[300px] w-[3/5] max-w-[90%] px-4 dark:text-gray-50 min-h-[75vh]">
